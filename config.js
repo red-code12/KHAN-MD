12192d0f5a7eb849a333beee2656d43cdafa4a46
red-code12 <earningw15@gmail.com>
2025-07-22 15:44:37 +0500

    Update config.js
    Me

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+if (fs.existsSync('config.env')) require('dotenv').config({ path: 'IK~RLEmhKBY#w-qYUZv2zFJMpJjAyASFBhXuzH2CRq0NVMjHKTesNa0' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
